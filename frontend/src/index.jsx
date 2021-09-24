d2044d7f418962fe5a5bc9bb71b74f672ea25c52
king-midas <mto0006@auburn.edu>
2021-09-24 01:10:19 -0500

    messing around

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -10,3 +10,5 @@ ReactDOM.render(<App />,  document.getElementById('root'));
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
---
+++ frontend/src/tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        sky: colors.yellow,
+        cyan: colors.cyan,
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
