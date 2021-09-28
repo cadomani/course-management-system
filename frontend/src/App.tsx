63aa6baba506dd5f13dd4f8d0a311b32e4154302
Carlos Domani <cad0087@auburn.edu>
2021-09-28 02:02:12 -0500

    Install tailwind with configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import "tailwindcss/tailwind.css"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" className="bg-green-500 rounded-md font-semibold shadow-md py-2 px-4" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
