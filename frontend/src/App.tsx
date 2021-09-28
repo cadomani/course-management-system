9bb4a5b7b91bf78d7869cd3ebdfeb75ab2b093fa
Carlos Domani <cad0087@auburn.edu>
2021-09-28 03:08:57 -0500

    Clean up structure and add headlessui elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,14 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 import "tailwindcss/tailwind.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" className="bg-green-500 rounded-md font-semibold shadow-md py-2 px-4" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <a href="/login">
+          <button className="bg-yellow-500 rounded-lg px-8 py-6 text-white font-extrabold hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">Click Me!</button>
+        </a>
+      </div>
     </div>
   )
 }
