b5581cb258b1a921c403af929b6313698b8544f6
Carlos Domani <cad0087@auburn.edu>
2021-10-21 19:45:35 -0500

    Allow receipt of multipart data

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,8 +4,9 @@ const auth = require('../services/auth');
 
 const router = new express.Router();
 
-router.use(express.urlencoded());
-router.post('/', async (req, res, next) => {
+router.use(express.urlencoded({ extended: true }));
+router.post('/', async (req, res) => {
+  // Parse body before authenticating
   const options = {
     email: req.body.email,
     password: req.body.password,
@@ -13,6 +14,9 @@ router.post('/', async (req, res, next) => {
 
   try {
     const result = await auth.authenticate(options);
+
+    // TODO: Return a session cookie with the private account key
+    // (for now just the username or public ID)
     res.status(result.status || 200).send(result.data);
   } catch (err) {
     return res.status(500).send({
--- backend/src/services/auth.js
+++ backend/src/services/auth.js
@@ -1,12 +1,15 @@
 module.exports = {
   /**
-  * Create a new course.
+  * Authenticate user based on credentials.
   */
   authenticate: async (options) => {
     const data = {
       email: options.email,
       password: options.password,
     };
+
+    // For now, we're simply ignoring the password. If a user exists in the database,
+    // they are returned regardless of the correctness of the password.
     const status = '200';
 
     return {
