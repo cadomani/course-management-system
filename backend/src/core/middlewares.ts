e162eab2c0f66a4d00292e150b1c337222e76687
Carlos Domani <cad0087@auburn.edu>
2021-10-27 16:37:57 -0500

    Temporarily disable notFound middleware

--- backend/src/core/middlewares.ts
+++ backend/src/core/middlewares.ts
@@ -5,22 +5,17 @@ import logger from '@shared/Logger';
 import StatusCodes from 'http-status-codes';
 const { BAD_REQUEST } = StatusCodes;
 
-const notFound = (req: Request, res: Response, next: NextFunction) => {
-  const error = new Error(`Not found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
+// export const notFound = (error: Error, req: Request, res: Response, next: NextFunction) => {
+//   const error = new Error(`Not found - ${req.originalUrl}`);
+//   res.status(404);
+//   next(error);
+// };
 
-const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   logger.err(error, true);
   res.status(statusCode).json({
     message: error.message,
     stack: process.env.NODE_ENV === 'production' ? 'None' : error.stack,
   });
-};
-
-module.exports = {
-  notFound,
-  errorHandler,
 };
