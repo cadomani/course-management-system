4d5b5359125092b61df1802a23771c3c80cd18b2
Carlos Domani <cad0087@auburn.edu>
2021-10-30 03:11:50 -0500

    Improve seed coverage. Profile, Instructor, College, Major, Department, and Course tables can be fully seeded.

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,70 +1,133 @@
-import { PrismaClient } from "@prisma/client";
-import csv from 'csv-parse';
+import { PrismaClient, Prisma } from "@prisma/client";
 import fs from 'fs';
-import { ml } from '../src/core/db/masterlist';
-const prisma = new PrismaClient();
+import parse from 'csv-parse';
+import * as _ from 'lodash';
 
-// function load_csv(): object[] {
-//   let results: any[] = [];
-//   fs.createReadStream('/server/school/cms/backend/src/core/db/masterlist_fall_v1.1.csv')
-//     .pipe(csv({
-//       quote: '`',
-//       delimiter: '|',
-//       columns: true
-//     }))
-//     .on('data', (data: any) => results.push(data))
-//     .on('end', () => {
-//       // console.log(results);
-//       // return results;
-//     })
-//   console.log(results);
-//   return results;
-// }
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+})
 
-// console.log(load_csv());
-// let masterlist: any = load_csv();
-// console.log(masterlist);
-// let collegesData : object[] = [];
-// masterlist.forEach((element: any) => {
-//   if (!collegesData.includes(element['name'])) {
-//     collegesData.push(element['name']);
-//   }
-//   console.log('a');
-// });
-// console.log(collegesData);
-// Array.from({length: 7726}).ml.forEach(element => {
-  
-// });
 
-// let collegesData = [];
-// ml.forEach((element: any) => {
-//   if (!collegesData.includes(element['name'])) {
-//     collegesData.push(element['name']);
-//   }
-// });
-// console.log(collegesData);
-// await prisma.college.create({
-//   data: {
-//     name: row['name']
-//   }
-// })
+const processFile = async () => {
+  var records = []
+  const parser = fs
+    .createReadStream(`/server/school/cms/backend/src/core/db/masterlist_fall_v1.1.csv`)
+    .pipe(parse({
+      quote: '`',
+      delimiter: '|',
+      columns: true
+    }));
+  for await (const record of parser) {
+    // Work with each record
+    records.push(record)
+  }
+  return records
+}
+
 
 async function main() {
-  // let filteredView: Set({ [key: string]: string }) = [{}];
-  // let filteredView = new Set();
-  // for (let row of ml) {
-  //   if (!filteredView.has({ name: row['college'] })) {
-  //     filteredView.add({
-  //       name: row['college'],
-  //     })
-  //   }
-  // }
-  // console.log(filteredView)
+  const records = await processFile();
+  let collegeFilteredView: any[] = [];
+  let courseFilteredView: any[] = [];
+  let profileInstructorDepartmentFV: any[] = [];
+  for (let row of records) {
+    if (row['college'] === '') {
+      console.log(row);
+    }
+    collegeFilteredView.push({
+      college: row['college'],
+      major: row['major'],
+    })
+    courseFilteredView.push({
+      college: row['college'],
+      major: row['major'],
+      course: row['name'],
+      credit_hours: Number.parseInt(row['credit_hours'])
+    })
+    if (row['instructors'] !== 'TBA' && row['instr_email'] !== '') {
+      profileInstructorDepartmentFV.push({
+        name: row['instructors'],
+        email: row['instr_email'],
+        college: row['college']
+      })
+    }
+  }
 
-  await prisma.college.createMany({
-    data: ml,
-    skipDuplicates: true,
-  })
+  // Load colleges and majors
+  let colleges = _.compact(_.uniqWith(collegeFilteredView, _.isEqual));
+  for (let c of colleges) {
+    await prisma.major.create({
+      data: {
+        name: c['major'],
+        college: {
+          connectOrCreate: {
+            where: {
+              name: c['college']
+            },
+            create: {
+              name: c['college']
+            }
+          }
+        }
+      }
+    })
+  }
+
+  // Load courses and majors
+  let courses = _.compact(_.uniqWith(courseFilteredView, _.isEqual));
+  for (let c of courses) {
+    let college = await prisma.college.findUnique({ where: { name: c['college'] }, select: {id: true} });
+    if (college === null) {
+      continue;
+    }
+    await prisma.course.create({
+      data: {
+        name: c['course'],
+        credit_hours: c['credit_hours'],
+        major: {
+          connect: {
+            uniqueMajor: {
+              name: c['major'],
+              college_id: college.id
+            }
+          }
+        }
+      }
+    })
+  }
+
+  // Load profiles, instructors, and departments
+  let instructors = _.compact(_.uniqWith(profileInstructorDepartmentFV, _.isEqual));
+  for (let c of instructors) {
+    let college = await prisma.college.findUnique({ where: { name: c['college'] }, select: { id: true } });
+    if (college === null) {
+      continue;
+    }
+
+    await prisma.instructor.create({
+      data: {
+        department: {
+          create: {
+            college_id: college.id
+          }
+        },
+        profile: {
+          connectOrCreate: {
+            where: {
+              email: c['email']
+            },
+            create: {
+              name: c['name'],
+              email: c['email'],
+              university: 'Auburn University'
+            }
+          }
+        }
+      }
+    })
+  }
+
+
 }
 
 main()
