c12b3b34f38284a8ad0c8bcebfad9055949c141a
Carlos Domani <cad0087@auburn.edu>
2021-10-31 19:18:04 -0500

    Set up registration and login routes

--- backend/src/services/auth.ts
+++ backend/src/services/auth.ts
@@ -1,30 +1,30 @@
-import prisma from '@shared/Database';
-import logger from '@shared/Logger';
+// import prisma from '@shared/Database';
+// import logger from '@shared/Logger';
 
-// ORM convenience mapping
-const User = prisma.profile;
+// // ORM convenience mapping
+// const User = prisma.profile;
 
-/**
- * Interfaces
- */
-interface Credentials {
-  email: string;
-  password: string;
-}
+// /**
+//  * Interfaces
+//  */
+// interface Credentials {
+//   email: string;
+//   password: string;
+// }
 
-/**
-* Authenticate user based on credentials.
-*/
-export async function authenticate(options: Readonly<Credentials>) {
-  const data = {
-    token: 'sampleToken',
-  };
-  // For now, we're simply ignoring the password. If a user exists in the database,
-  // they are returned regardless of the correctness of the password.
-  const status = 200;
+// // /**
+// // * Authenticate user based on credentials.
+// // */
+// // export async function authenticate(options: Readonly<Credentials>) {
+// //   const data = {
+// //     token: 'sampleToken',
+// //   };
+// //   // For now, we're simply ignoring the password. If a user exists in the database,
+// //   // they are returned regardless of the correctness of the password.
+// //   const status = 200;
 
-  return {
-    data,
-    status,
-  };
-}
+// //   return {
+// //     data,
+// //     status,
+// //   };
+// // }
