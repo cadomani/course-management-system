9d7e8b669afc7fe9a5702c2681f60f4e08991fe7
Carlos Domani <cad0087@auburn.edu>
2021-11-02 02:38:04 -0500

    Filter user routes to match new api schema (non functional)

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,14 +1,14 @@
 import logger from '@shared/Logger';
 import { Router, urlencoded } from 'express';
-import { listUsers, createUser, replaceUser, updateUser, getUserProfile, updateUserProfile, getUserUUID } from '../services/user';
+import { getUserProfile, updateUserProfile, getUserProfilePhoto, replaceUserProfilePhoto } from '../services/user';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 const router = Router();
 router.use(urlencoded({ extended: true }));
 
-router.get('/', async (req, res, next) => {
+router.get('/user/:id/profile', async (req, res, next) => {
   try {
-    const result = await listUsers();
+    const result = await getUserProfile(req.body.id);
     res.status(result.status || 200).send(result.data);
   } catch (err) {
     return res.status(500).send({
@@ -18,142 +18,15 @@ router.get('/', async (req, res, next) => {
   return res;
 });
 
-/**
- * Create a new user, either as an administrator or as a routine through registration.
- * major_id and college_id form data are numerical to access specific records and are mapped from course listing.
- */
-router.post('/', async (req, res, next) => {
-  const options = {
-    name: req.body.name,
-    email: req.body.email,
-    biography: req.body.biography,
-    university: process.env.UNIVERSITY as string,
-    password: 'NOT_IMPLEMENTED', // TODO: Need hashing and salting before using this
-    major_id: req.body.major_id as number,
-    college_id: req.body.college_id as number,
-  };
 
+router.patch('/user/:id/profile', async (req, res, next) => {  
   try {
-    const result = await createUser(options);
-    res.status(result.status || 200).send(result.data);
-  } catch (err) {
-    if (err instanceof PrismaClientKnownRequestError) {
-      if (err.code === 'P2002') {
-        logger.err('A user already exists with this email.');
-        return res.status(403).send({
-          status: 'failure',
-          detail: 'An account already exists with this email address. Please log in instead.'
-        });
-      }
-    }
-    return res.status(500).send({
-      error: err || 'Something went wrong.',
-    });
-  }
-  return res;
-});
-
-router.put('/:id', async (req, res, next) => {
-  const identity = {
-    publicId: req.params.id as string,
-  }
-
-  const options = {
-    name: req.body.name,
-    email: req.body.email,
-    biography: req.body.biography,
-    university: process.env.UNIVERSITY as string,
-    password: 'NOT_IMPLEMENTED', // TODO: Need hashing and salting for this
-    major_id: req.body.major_id,
-    college_id: req.body.college_id,
-  };
-
-  try {
-    const result = await replaceUser(identity, options);
-    res.status(result.status || 200).send(result.data);
-  } catch (err) {
-    return res.status(500).send({
-      error: err || 'Something went wrong.',
-    });
-  }
-  return res;
-});
-
-router.patch('/:id', async (req, res, next) => {
-  // Check for presence of UUID header
-  const identity = {
-    publicId: req.params.id,
-  }
-
-  const options = {
-    name: req.body.name,
-    biography: req.body.biography,
-    university: process.env.UNIVERSITY as string,
-    college_id: req.body.college,
-  };
-
-  try {
-    const result = await updateUser(identity, options);
-    res.status(result.status || 200).send(result.data);
-  } catch (err) {
-    return res.status(500).send({
-      error: err || 'Something went wrong.',
-    });
-  }
-  return res;
-});
-
-router.get('/:id/profile', async (req, res, next) => {
-  // Check for presence of UUID header
-  // const uuid = req.headers['x-user-uuid'];
-  // if (typeof (uuid) === 'object' || typeof (uuid) === 'undefined') {
-  //   logger.err('This resource cannot be retrieved: X-USER-UUID header missing.');
-  //   return {
-  //     status: 400,
-  //     data: "This resource cannot be retrieved: X-USER-UUID header missing."
-  //   };
-  // }
-
-  const identity = {
-    publicId: req.params.id,
-    // uuid: uuid,
-  }
-
-  try {
-    const result = await getUserProfile(identity);
-    res.status(result.status || 200).send(result.data);
-  } catch (err) {
-    res.status(500).send({
-      error: err || 'Something went wrong.',
-    });
-  }
-});
-
-router.patch('/:id/profile', async (req, res, next) => {
-  // Check for presence of UUID header and verify JWT value before continuing
-  const uuid = req.headers['x-user-uuid'];
-  if (typeof (uuid) === 'object' || typeof (uuid) === 'undefined') {
-    logger.err('This resource cannot be retrieved: X-USER-UUID header missing.');
-    return {
-      status: 400,
-      data: "This resource cannot be retrieved: X-USER-UUID header missing."
+    const options = {
+      id: req.body.id,
+      name: req.body.name,
+      biography: req.body.biography,
     };
-  }
-
-  const identity = {
-    publicId: req.params.id,
-    uuid: uuid,
-  }
-
-  const options = {
-    name: req.body.name,
-    biography: req.body.biography,
-    university: process.env.UNIVERSITY as string,
-    college_id: req.body.college,
-  };
-
-  try {
-    const result = await updateUserProfile(identity, options);
+    const result = await updateUserProfile(options);
     res.status(result.status || 200).send(result.data);
   } catch (err) {
     return res.status(500).send({
@@ -163,15 +36,21 @@ router.patch('/:id/profile', async (req, res, next) => {
   return res;
 });
 
-router.get('/:id/uuid', async (req, res, next) => {
-  // TODO: Validate JWT before continuing
-  const identity = {
-    publicId: req.params.id,
-    uuid: undefined
-  };
-
+router.get('/user/:id/profilePhoto', async (req, res, next) => {
   try {
-    const result = await getUserUUID(identity);
+    const result = await getUserProfilePhoto(req.body.id);
+    res.status(result.status || 200).send(result.data);
+  } catch (err) {
+    return res.status(500).send({
+      error: err || 'Something went wrong.',
+    });
+  }
+  return res;
+});
+
+router.put('/user/:id/profilePhoto', async (req, res, next) => {
+  try {
+    const result = await replaceUserProfilePhoto(req.body.id);
     res.status(result.status || 200).send(result.data);
   } catch (err) {
     return res.status(500).send({
--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -3,10 +3,7 @@ import prisma from '@shared/Database';
 import logger from '@shared/Logger';
 import {
   ProfileIdentity,
-  CreateProfile,
   UpdateProfile,
-  CreateStudentProfile,
-  CreateInstructorProfile,
   QualifiedProfile,
   NonQualifiedProfile,
   ResponseFormat
@@ -15,182 +12,10 @@ import {
 // ORM convenience mapping
 const User = prisma.profile;
 
-
-/**
- * DEBUG: List all users on the database
- * Access: Restricted
- * @param options 
- * @returns 
- */
-export async function listUsers(): Promise<ResponseFormat> {
-  const data = await User.findMany({
-    select: {
-      name: true,
-      email: true,
-      biography: true,
-      university: true,
-      credentials: {
-        select: {
-          last_login: true,
-          signup_date: true,
-        }
-      }
-    },
-  });
-
-  // Send success in either case
-  return {
-    status: 200,
-    data: (data === null || Object.keys(data).length == 0) ? [] : data,
-  };
-}
-
-/**
- * Add a user to the database
- * Access: Restricted
- * @param options 
- * @returns 
- */
-export async function createUser(
-  options: CreateProfile
-): Promise<ResponseFormat> {
-  // Create the base profile from options
-  let profile: Prisma.profileCreateInput = {
-    name: options.name,
-    email: options.email,
-    biography: options.biography,
-    university: options.university,
-    credentials: {
-      create: {
-        password_hash: options.password,
-        signup_date: '1970-01-01T00:00:00.000Z',
-      }
-    },
-  };
-
-  if (options as CreateStudentProfile) {
-    profile.student = {
-      create: {
-        major: {
-          connect: {
-            id: 1,
-          }
-        }
-      }
-    }
-  } else if (options as CreateInstructorProfile) {
-    profile.instructor = {
-      create: {
-        department: {
-          connect: {
-            id: 1
-          }
-        }
-      }
-    }
-  }
-
-  const data = await User.create({
-    data: profile
-  })
-
-  return {
-    status: 201,
-    data: {
-      publicId: data.id,
-    },
-  };
-}
-
-/**
- * Replace user in database
- * Access: Restricted
- * @param options 
- * @returns 
- */
-export async function replaceUser(
-  profile: NonQualifiedProfile,
-  options: CreateProfile,
-): Promise<ResponseFormat> {
-  // Create the base profile from options
-  let newProfile: Prisma.profileUpdateInput = {
-    name: options.name,
-    email: options.email,
-    biography: options.biography,
-    university: options.university,
-    credentials: {
-      update: {
-        password_hash: options.password,
-        signup_date: '1970-01-01T00:00:00.000Z',
-      }
-    }
-  };
-
-  // if (options as CreateStudentProfile) {
-  //   newProfile.student = {
-  //     update: {
-  //       where: {
-  //         id: 4,
-  //       }
-  //     }
-  //   }
-  // } else if (options as CreateInstructorProfile) {
-  //   newProfile.instructor = {
-  //     update: {
-  //       department: {
-  //         connect: {
-  //           id: 1
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
-  const data = await User.update({
-    where: {
-      id: Number.parseInt(profile.publicId)
-    },
-    data: newProfile
-  })
-
-  return {
-    status: 200,
-    data: {
-      publicId: data.id,
-    },
-  };
-}
-
-/**
- * Update a user already in the database
- * Access: Restricted
- * @param options 
- * @returns 
- */
-export async function updateUser(
-  profile: NonQualifiedProfile,
-  options: UpdateProfile
-): Promise<ResponseFormat> {
-  const data = {
-    detail: 'success',
-    status: 'operation was successful',
-  };
-  const status = 200;
-
-  return {
-    status,
-    data,
-  };
-}
-
 /**
  * Retrieve the user profile
- * @param options 
- * @returns 
  */
-export async function getUserProfile(
-  identity: ProfileIdentity,
-): Promise<ResponseFormat> {
+export async function getUserProfile(identity: ProfileIdentity): Promise<ResponseFormat> {
   // Obtain data for profiles
   // TODO: Unstable behavior - type predicates don't seem to work when interfaces contain the same properties
   let data;
@@ -230,17 +55,44 @@ export async function getUserProfile(
 
 /**
  * Update the user profile
- * @param options 
- * @returns 
  */
-export async function updateUserProfile(
-  identity: QualifiedProfile,
-  options: UpdateProfile
-): Promise<ResponseFormat> {
-  const data = {
-    detail: 'success',
-    status: 'operation was successful',
+export async function updateUserProfile(options: any): Promise<ResponseFormat> {
+  // Create the base profile from options
+  let newProfile: Prisma.profileUpdateInput = {
+    name: options.name,
+    biography: options.biography,
   };
+
+  const data = await User.update({
+    where: {
+      id: options.id
+    },
+    data: newProfile
+  })
+
+  return {
+    status: 200,
+    data: {
+      publicId: data.id,
+    },
+  };
+}
+
+
+/**
+* Replace public profile photo of a user
+*/
+export async function getUserProfilePhoto(options: string) {
+  // Check for presence of UUID header
+  if (options == undefined) {
+    logger.err('This resource cannot be retrieved.');
+    return {
+      status: 400,
+      data: "This resource cannot be retrieved."
+    };
+  }
+
+  const data = {};
   const status = 200;
 
   return {
@@ -250,33 +102,23 @@ export async function updateUserProfile(
 }
 
 /**
- * Retrieve the private uuid of a user for authenticated requests
- * Tolerant against requests for already qualified profiles
- * @returns 
- */
-export async function getUserUUID(
-  identity: ProfileIdentity
-): Promise<ResponseFormat> {
-  const data = await User.findFirst({
-    where: {
-      id: Number.parseInt(identity.publicId)
-    },
-    // select: {
-    //   //uuid: true
-    // }
-  })
-  if (data === null) {
+* Replace public profile photo of a user
+*/
+export async function replaceUserProfilePhoto(options: string) {
+  // Check for presence of UUID header
+  if (options === undefined) {
+    logger.err('This resource cannot be retrieved');
     return {
-      status: 404,
-      data: {},
+      status: 400,
+      data: "This resource cannot be retrieved."
     };
   }
+  const data = {};
+  const status = 200;
 
   return {
-    status: 200,
-    data: {
-      publicId: identity.publicId,
-      uuid: 'NOT_IMPLEMENTED' // TODO: uuid column needs to be added to table - data.uuid
-    },
+    status,
+    data,
   };
 }
+
