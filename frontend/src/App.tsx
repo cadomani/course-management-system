b8874c290462ebd6030e6c7ddb811b4840e9b639
Carlos Domani <cad0087@auburn.edu>
2021-11-11 16:08:19 -0600

    Minor cleanup and readability changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,8 @@ import {
 } from "react-router-dom";
 import BackgroundSlider from 'react-background-slider' //https://www.npmjs.com/package/react-background-slider
 
+// TODO: Import as a group and cycle all available images instead of importing individually
+// TODO: Provide attribution and credits for photos
 // Background images
 import bg1 from '../images/samford-bg.jpg';
 import bg2 from '../images/au-bg2.png';
@@ -17,11 +19,11 @@ import bg3 from '../images/au-bg3.png';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Link to="/login">Login</Link> |{" "}
-    <Link to="/registration">Register</Link> |{" "}
-    <Link to="/majorselection">Majors</Link>
     <Grid>
       <GridItem colSpan={1}>
+        <Link to="/login"><b>Login</b></Link> |{" "}
+        <Link to="/registration"><b>Registration</b></Link> |{" "}
+        <Link to="/majorselection"><b>Majors</b></Link>
         <Outlet />
       </GridItem>
       <GridItem colSpan={2}>
