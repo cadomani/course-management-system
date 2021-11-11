e2a1cc03353063ad136e94e7665e9aa6a7597fed
king-midas <mto0006@auburn.edu>
2021-11-11 01:24:55 -0600

    Added email input

--- frontend/src/components/registration/RegistrationPage.tsx
+++ frontend/src/components/registration/RegistrationPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState } from 'react';
 import {
   Container,
   Grid,
@@ -13,6 +13,7 @@ import {
   FormLabel,
   FormErrorMessage,
   FormHelperText,
+  Input,
 } from "@chakra-ui/react"
 
 export default function RegistrationPage() {
@@ -22,12 +23,24 @@ export default function RegistrationPage() {
       templateColumns="repeat(3, 1fr)"
     >
       <GridItem colSpan={1}>
-        <Center w="40vw" h="97vh" bg="white">
+        <Center w="40vw" h="97vh" bg="teal">
           
+        <FormControl id="email">
+          <FormLabel>Name</FormLabel>
+          <Input type="email" />
+          <FormHelperText>We'll never share your email.</FormHelperText>
+          </FormControl>
+          
+        <FormControl id="email">
+        <FormLabel>Email address</FormLabel>
+        <Input type="email" />
+        <FormHelperText>We'll never share your email.</FormHelperText>
+         </FormControl>
+
         </Center>
       </GridItem>
       <GridItem colSpan={2}>
-        <Center w="60vw" h="97vh" bg="white">
+        <Center w="60vw" h="97vh" bg="blue">
           <Text>Registration Welcome Image</Text>
         </Center>
       </GridItem>
