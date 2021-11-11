95a703b7cb9fadf1abdd65a04f72089e5dd9071d
Carlos Domani <cad0087@auburn.edu>
2021-11-10 20:52:16 -0600

    Delete unnecessary files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,16 +8,11 @@ import {
   Grid,
   theme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
-
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
           <Link
             color="teal.500"
             href="/login.html"
--- frontend/src/forgotpass.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-import "tailwindcss/tailwind.css"
-
-export function App() {
-  return (
-    <div></div>
-  )
-}
