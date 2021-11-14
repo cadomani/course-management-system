7a35937ac82f837add112e56592162ce4ed1a032
Carlos Domani <cad0087@auburn.edu>
2021-11-14 04:30:34 -0600

    Re-enable sentry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,30 +6,29 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-// import * as Sentry from "@sentry/react";
-// import { Integrations } from "@sentry/tracing";
-// import { RewriteFrames } from "@sentry/integrations";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import { RewriteFrames } from "@sentry/integrations";
 import LoginPage from './components/login/LoginPage';
 import RegistrationPage from './components/registration/RegistrationPage';
 
-// const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
-// const SENTRY_ENVIRONMENT = import.meta.env.VITE_SENTRY_ENVIRONMENT;
-// const HEROKU_ROOT = import.meta.env.VITE_HEROKU_ROOT_FRONTEND_DIR;
-// if (typeof (SENTRY_DSN) !== 'string' || typeof (SENTRY_ENVIRONMENT) !== 'string' || typeof (HEROKU_ROOT) !== 'string') {
-//   throw new Error('Sentry environment variables are missing.')
-// } else {
-//   console.log(SENTRY_DSN);
-//   Sentry.init({
-//     dsn: SENTRY_DSN,
-//     integrations: [
-//       new Integrations.BrowserTracing(),
-//       new RewriteFrames({
-//         root: HEROKU_ROOT
-//       })],
-//     tracesSampleRate: 1.0,
-//     environment: SENTRY_ENVIRONMENT
-//   })
-// }
+const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
+const SENTRY_ENVIRONMENT = import.meta.env.VITE_SENTRY_ENVIRONMENT;
+const HEROKU_ROOT = import.meta.env.VITE_HEROKU_ROOT_FRONTEND_DIR;
+if (typeof (SENTRY_DSN) !== 'string' || typeof (SENTRY_ENVIRONMENT) !== 'string' || typeof (HEROKU_ROOT) !== 'string') {
+  throw new Error('Sentry environment variables are missing.')
+} else {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [
+      new Integrations.BrowserTracing(),
+      new RewriteFrames({
+        root: HEROKU_ROOT
+      })],
+    tracesSampleRate: 1.0,
+    environment: SENTRY_ENVIRONMENT
+  })
+}
 
 ReactDOM.render(
   <BrowserRouter>
