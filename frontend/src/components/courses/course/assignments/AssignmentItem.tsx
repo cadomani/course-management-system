8f17c687970e3687ebf785e5a9db1591d0aff60c
Isaiah Burkes <idkburkes@gmail.com>
2021-11-17 21:27:36 -0600

    Created StudentAssignmentItem component

--- frontend/src/components/courses/course/assignments/AssignmentItem.tsx
+++ frontend/src/components/courses/course/assignments/AssignmentItem.tsx
@@ -2,6 +2,20 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import axios from 'axios';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box, 
+  Checkbox,
+  Text,
+  Textarea,
+  Button,
+  useToast,
+  UseToastOptions
+} from "@chakra-ui/react"
 const DOMAIN = import.meta.env.VITE_DOMAIN;
 
 /**
@@ -15,3 +29,73 @@ export default function AssignmentItem() {
     </>
   )
 }
+
+
+export function StudentAssignmentItem() {
+  const [value, setValue] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [submitToast, setSubmitToast] = useState<UseToastOptions>();
+
+  const toast = useToast();
+
+  let handleInputChange = (e : any) => {
+    let inputValue = e.target.value
+    setValue(inputValue)
+  }
+
+  let handleSubmitButtonPressed = () => {
+    console.log('Submitting assignment: ' + {value})
+    setSubmitted(true);
+    toast({
+      title: 'Assignment submitted!',
+      description: 'Instructor has been notified of your assignment submission.',
+      status: 'success',
+      isClosable: true,
+      position: 'bottom'
+    });
+  }
+
+  // Return component
+  return (
+    <Box w="50vw">
+    <Accordion defaultIndex={[0]} allowMultiple>
+  <AccordionItem>
+    <h2>
+      <AccordionButton bg='grey'>
+        <Box flex="1" textAlign="left">
+          Assignment 1
+        </Box>
+        <Box flex="1" textAlign="right">
+           <Checkbox colorScheme="green" isReadOnly isChecked={submitted}>
+           {submitted ? 'Submitted' : 'Due 11/18/21'}
+            </Checkbox>
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+    <Text mb="8px"> Assignment instructions that were created by the instructor.</Text>
+      <Textarea
+        value={value}
+        onChange={handleInputChange}
+        placeholder="Type your answers here."
+        size="sm"
+        resize="none"
+      />
+      <Box flex="1" textAlign="right">
+      <Button
+       colorScheme="teal"
+       size="xs"
+       type="submit"
+       onClick={handleSubmitButtonPressed}>
+         Submit
+      </Button>
+        </Box>
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
+</Box>
+  )
+}
+
+
