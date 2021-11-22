b180538a65ee7fd10e6c3b6a9dd7ea8ceb1a2019
Carlos Domani <carlos.a.domani@gmail.com>
2021-11-22 04:13:16 +0000

    Add production sentry back to frontend and conditionally enable

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,9 +6,9 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-// import * as Sentry from "@sentry/react";
-// import { Integrations } from "@sentry/tracing";
-// import { RewriteFrames } from "@sentry/integrations";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import { RewriteFrames } from "@sentry/integrations";
 
 // Views
 import './static/css/index.css';
@@ -17,23 +17,27 @@ import LoginPage from './components/login/LoginPage';
 import RegistrationPage from './components/registration/RegistrationPage';
 import DashboardPage from './components/DashboardPage';
 
-// const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
-// const SENTRY_ENVIRONMENT = import.meta.env.VITE_SENTRY_ENVIRONMENT;
-// const HEROKU_ROOT = import.meta.env.VITE_HEROKU_ROOT_FRONTEND_DIR;
-// if (typeof (SENTRY_DSN) !== 'string' || typeof (SENTRY_ENVIRONMENT) !== 'string' || typeof (HEROKU_ROOT) !== 'string') {
-//   throw new Error('Sentry environment variables are missing.')
-// } else {
-//   Sentry.init({
-//     dsn: SENTRY_DSN,
-//     integrations: [
-//       new Integrations.BrowserTracing(),
-//       new RewriteFrames({
-//         root: HEROKU_ROOT
-//       })],
-//     tracesSampleRate: 1.0,
-//     environment: SENTRY_ENVIRONMENT
-//   })
-// }
+const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
+const SENTRY_ENVIRONMENT = import.meta.env.VITE_SENTRY_ENVIRONMENT;
+const SENTRY_ENABLE = import.meta.env.VITE_SENTRY_ENABLE || "false";
+const HEROKU_ROOT = import.meta.env.VITE_HEROKU_ROOT_FRONTEND_DIR;
+if (typeof SENTRY_ENABLE !== 'string' || SENTRY_ENABLE === 'false') {
+  console.log('Sentry disabled (development)');
+} else if (typeof SENTRY_DSN !== 'string' || typeof SENTRY_ENVIRONMENT !== 'string' || typeof HEROKU_ROOT !== 'string') {
+  console.log('Sentry environment variables are missing.');
+} else {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [
+      new Integrations.BrowserTracing(),
+      new RewriteFrames({
+        root: HEROKU_ROOT,
+      }),
+    ],
+    tracesSampleRate: 1.0,
+    environment: SENTRY_ENVIRONMENT,
+  });
+}
 
 ReactDOM.render(
   <BrowserRouter>
