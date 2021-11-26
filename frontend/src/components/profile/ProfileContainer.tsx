d67c2c31fc24ac21fa366e27a143e7f2b11dbfca
Micah Still <mbs0024@auburn.edu>
2021-11-25 21:54:23 -0600

    Added a few profile details

--- frontend/src/components/profile/ProfileContainer.tsx
+++ frontend/src/components/profile/ProfileContainer.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import axios from 'axios';
+import { Button, Flex, Center, Text } from "@chakra-ui/react"
 const DOMAIN = import.meta.env.VITE_DOMAIN;
 
 /**
@@ -9,10 +10,38 @@ const DOMAIN = import.meta.env.VITE_DOMAIN;
  */
 export default function ProfileContainer() {
 
-  // Return component
+  /*
+   * Return component
+   * Make image changeable? Put a variable instead of
+   * hardcoding the image, and allow user to change.
+   * Replace username with user's name when passing it in.
+   * Can't get it to not shove a tiny bit when switching.
+   * Does not switch back. Error with Profile Button?
+   */
   return (
     <>
-      <h1>Profile</h1>
+      <Flex direction="row" height="92.9vh" alignItems="flex-start" flexWrap="nowrap" justifyContent="center">
+        <Flex direction="column" height="100%" alignItems="center" justifyContent="space-between">
+          <h1>
+            <div className="centerPic">
+              <img src="../../images/profilestock.jpg"
+                alt="Profile Picture"
+              />
+            </div>
+            <Text fontSize="sm" fontFamily="Montserrat, sans-serif" fontWeight="regular" paddingBottom="15px">
+              Username
+            </Text>
+          </h1>
+          <div>
+            <Center>
+              <Button size="xs">Change Picture</Button>
+            </Center>
+            <Center>
+              <Button size="xs">Change Display Name</Button>
+            </Center>
+          </div>
+        </Flex>
+      </Flex>
     </>
   )
 }
