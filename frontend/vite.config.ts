c4eb137f24d3551654c27c919516dbf27dfe0933
Carlos Domani <carlos.a.domani@gmail.com>
2021-11-26 11:27:07 +0000

    Add default port to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5080,
+  },
   build: {
     rollupOptions: {
       input: {
